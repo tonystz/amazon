d1dbece44a8d031bc69e17feeaa81cbe2377daa5
Tony Su <tony_su@trendmicro.com>
2022-07-23 22:37:31 +0800

    add data

--- inputs.py
+++ inputs.py
@@ -8,4 +8,4 @@ year=[2016,2022]
 #product_url = 'https://www.amazon.com/Headlight-SEALIGHT-Design-Upgraded-Warranty/dp/B07HCZ7RZ6'
 #product_url = 'https://www.amazon.com/Fahren-Headlight-Headlights-Conversion-Waterproof/dp/B07NPMV1H5'
 #product_url = 'https://www.amazon.com/Puroma-Filter-Activated-Replacement-CF10285/dp/B07VHMX2NT'
-product_url = 'https://www.amazon.com/SEALIGHT-360-degree-Illumination-Brightness-Installation/dp/B07Q24LVDM'
+product_url = 'https://www.amazon.com/SEALIGHT-360-degree-Illumination-Brightness-Installation/dp/B093GVXLDZ'
