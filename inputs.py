87017e3305563fba9d5401c406ab52cd57bea672
Tony Su <tony_su@trendmicro.com>
2023-03-05 11:37:36 +0800

    update

--- inputs.py
+++ inputs.py
@@ -2,7 +2,7 @@
 year=[1896, 1897, 1898, 1899, 1900, 1901, 1902, 1903, 1904, 1905, 1906, 1907, 1908, 1909, 1910, 1911, 1912, 1913, 1914, 1915, 1916, 1917, 1918, 1919, 1920, 1921, 1922, 1923, 1924, 1925, 1926, 1927, 1928, 1929, 1930, 1931, 1932, 1933, 1934, 1935, 1936, 1937, 1938, 1939, 1940, 1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949, 1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
 # year=[2009]
 # from 2016 to 2022
-year=[2012,2023]
+year=[2005,2022]
 
 #product_url = 'https://www.amazon.com/Forenner-Headlight-Conversion-Installation-Replacement/dp/B096812T9V'
 #product_url = 'https://www.amazon.com/Headlight-SEALIGHT-Design-Upgraded-Warranty/dp/B07HCZ7RZ6'
@@ -22,4 +22,4 @@ year=[2012,2023]
 #2005 ~2022
 product_url='https://www.amazon.com/EPAuto-CF179C-Replacement-Premium-Activated/dp/B01LYT1VCU'
 #2012~2023
-product_url='https://www.amazon.com/Spearhead-Premium-Breathe-Filter-Activated/dp/B08F33RHRJ'
+#product_url='https://www.amazon.com/Spearhead-Premium-Breathe-Filter-Activated/dp/B08F33RHRJ'
